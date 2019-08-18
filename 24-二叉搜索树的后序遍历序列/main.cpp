6b256d5d35eedd93139db503a89f7b2a51ca96dc
zhanlaoban <zhanlaoban@gmail.com>
2019-08-18 20:47:23 +0800

    update

---
+++ 24-二叉搜索树的后序遍历序列/main.cpp
@@ -0,0 +1,84 @@
+/*
+    思考：本题需要建树吗？不需要，只要后序序列满足那种特性即可。
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//自己写的方法，牛客网报错如下：
+/*
+    答案错误:您提交的程序没有通过所有的测试用例
+    case通过率为87.50%
+
+    用例:
+    []
+
+    对应输出应该为:
+
+    false
+
+    你的输出为:
+
+    true
+*/
+//应如何修改？
+class Solution {
+public:
+    bool VerifySquenceOfBST(vector<int> sequence) {
+        
+        if( sequence.size() == 0 ){
+            return true;
+        }
+        
+        int lastPos = sequence.size();
+        int root = sequence[lastPos-1];
+        cout << root << endl;
+
+        vector<int> left;
+        vector<int> right;
+
+        int rightStart = -1;
+        for(int i = 0; i < sequence.size(); i++){
+            if( sequence[i] < root ){
+                left.push_back(sequence[i]);
+            }else{
+                rightStart = i;
+                break;
+            }
+        }
+        for(int i = rightStart; i < sequence.size() - 1; i++){
+            if( (sequence[i] > root) == false ){
+                return false;
+            }
+            right.push_back(sequence[i]);
+        }
+
+
+        return VerifySquenceOfBST(left) && VerifySquenceOfBST(right);
+        
+    }
+};
+
+int main(){
+
+    vector<int> sequence;
+    //int postOrder[] = {5, 7, 6, 9, 11, 10, 8};
+    int postOrder[] = {7, 4, 6, 5};
+    
+    //将后续遍历序列放进sequence中
+    for(int i = 0; i < 7; i++ ){
+        sequence.push_back(postOrder[i]);
+    }
+
+
+    Solution solution;
+    if (solution.VerifySquenceOfBST(sequence)) {
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
+    
+
+    return 0;
+}
