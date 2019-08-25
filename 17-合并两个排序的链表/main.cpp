3a7f9a0813ab312ba78454993062149fdc24cbd4
zhanlaoban <zhanlaoban@gmail.com>
2019-08-25 10:33:20 +0800

    update solution

--- 17-合并两个排序的链表/main.cpp
+++ 17-合并两个排序的链表/main.cpp
@@ -19,9 +19,16 @@ struct ListNode{
     后移后，再进行比较
     最后，对p1和p2判空，若不空（肯定有一个为空，有一个不空），则加入新的链表中。
 */
-class Solution{
+class Solution1{
 public:
     ListNode* Merge(ListNode* pHead1, ListNode* pHead2){
+        if( pHead1 == NULL ){
+            return pHead2;
+        }
+        if( pHead2 == NULL ){
+            return pHead1;
+        }
+
         //创建新链表头结点
         ListNode *pHead = new ListNode(-1);
         pHead->next = NULL;
@@ -72,6 +79,36 @@ public:
         
     }
 
+};
+
+
+/*
+    另一种方法:
+    step1.先判断边界
+    step2.从头遍历链表，判断当前指针，哪个链表中的值小，即赋给合并链表指针即可。使用递归实现。
+*/
+class Solution {
+public:
+    ListNode* Merge(ListNode* pHead1, ListNode* pHead2){
+        //判断指针是否为空
+        if(pHead1 == NULL){
+            return pHead2;
+        }
+        else if(pHead2 == NULL){
+            return pHead1;
+        }
+
+        ListNode* pMergedHead = NULL;
+        if(pHead1->val < pHead2->val){
+            pMergedHead = pHead1;
+            pMergedHead->next = Merge(pHead1->next, pHead2);
+        }else{
+            pMergedHead = pHead2;
+            pMergedHead->next = Merge(pHead1, pHead2->next);
+        }
+
+        return pMergedHead;
+    }
 };
 
 int main(){
